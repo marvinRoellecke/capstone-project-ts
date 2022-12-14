cd82bb5eb9cccc833aac6a9f684daa3af4317ff6
Marvin Roellecke <marvin@roellecke.com>
2022-12-14 15:06:39 +0100

    create [slug].js -> DetailsPage

---
+++ pages/[slug].js
@@ -0,0 +1,6 @@
+import testItemData from "../../utils/data/testItemData";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+export default function DetailsPage() {}
