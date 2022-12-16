756eb088c671928f91eaebddbf2ce095bef586ab
Marvin Roellecke <marvin@roellecke.com>
2022-12-16 11:06:15 +0100

    remove sort button. only use filter button to show filter and sort menu

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,47 +1,32 @@
 import styled from "styled-components";
-import { IoOptionsOutline, IoSwapVertical } from "react-icons/io5";
+import { IoOptionsOutline } from "react-icons/io5";
 
 export default function Header() {
   return (
     <StyledHeader>
       <StyledH1>localSports</StyledH1>
-      <div>
-        <StyledButton>
-          <StyledSortButton />
-        </StyledButton>
-        <StyledButton>
-          <StyledFilterButton />
-        </StyledButton>
-      </div>
+      <StyledButton>
+        <StyledFilterButton />
+      </StyledButton>
     </StyledHeader>
   );
 }
+
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin: 0 1rem 1rem 1rem;
+`;
 
 const StyledH1 = styled.h1`
   width: fit-content;
   height: fit-content;
 `;
 
-const StyledHeader = styled.header`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-
-  div {
-    display: flex;
-    justify-content: space-between;
-    gap: 0.5rem;
-  }
-`;
-
 const StyledButton = styled.button`
   background: none;
   border: none;
-`;
-
-const StyledSortButton = styled(IoSwapVertical)`
-  width: auto;
-  height: 1.5rem;
 `;
 
 const StyledFilterButton = styled(IoOptionsOutline)`
