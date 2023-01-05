3e95c2687b766773ebf4ed3d8e10327bb573a7e8
Marvin Roellecke <marvin@roellecke.com>
2023-01-05 23:37:30 +0100

    add filter function to GET request api/locations/index.js

--- pages/api/locations/index.js
+++ pages/api/locations/index.js
@@ -5,7 +5,18 @@ export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
-    const locations = await Location.find();
+    const filter = {};
+
+    if (request.query.sport) {
+      filter["info.sport"] = request.query.sport;
+    }
+    if (request.query.rating) {
+      filter.rating = { $gte: request.query.rating };
+    }
+    if (request.query.city) {
+      filter["address.city"] = request.query.city;
+    }
+    const locations = await Location.find(filter);
 
     const locationsArray = locations.map((location) => {
       return {
