ef5b2e32e3c85da726488d380986a07be11c4b06
Marvin Roellecke <marvin@roellecke.com>
2023-01-18 18:48:43 +0100

    style card on details page sport info

--- components/CardLocationInfo/CardLocationInfo.js
+++ components/CardLocationInfo/CardLocationInfo.js
@@ -15,51 +15,37 @@ export default function CardLocationInfo({ currentLocation }) {
               height={35}
             />
             <span>{sport}</span>
-          </StyledBox>
-          <StyledBox>
-            <Image
-              src={`/img/sportLogos/icon-basketball-field.png`}
-              alt="Logo of court"
-              width={40}
-              height={40}
-            />
-            <span>
-              {numberOfCourts === 0
-                ? "n/a"
-                : numberOfCourts === 1
-                ? "1 Spielfeld"
-                : numberOfCourts + " Spielfelder"}
-            </span>
-            <span>{surface !== "" ? surface : "n/a"}</span>
+            <h3>{numberOfCourts === 0 ? "n/a" : numberOfCourts}</h3>
+            <span>{numberOfCourts === 1 ? "Spielfeld" : " Spielfelder"}</span>
+            <h3>{surface !== "" ? surface : "n/a"}</h3>
+            <span>Untergrund</span>
           </StyledBox>
         </StyledBoxWrapper>
       ))}
 
       <StyledDivider />
-      <StyledBoxWrapper>
-        <StyledBox>
+      <StyledBoxOtherWrapper>
+        <StyledBoxOther>
           <Image
             src={`/img/sportLogos/icon-light.png`}
             alt="Logo of sport"
             width={35}
             height={35}
           />
-          <h3>Beleuchtung</h3>
-          <span>{currentLocation.infrastructure.lighting ? "ja" : "nein"}</span>
-        </StyledBox>
-        <StyledBox>
+          <h3>{currentLocation.infrastructure.lighting ? "ja" : "nein"}</h3>
+          <span>Beleuchtung</span>
+        </StyledBoxOther>
+        <StyledBoxOther>
           <Image
             src={`/img/sportLogos/icon-wheelchair.png`}
             alt="Logo of sport"
             width={35}
             height={35}
           />
-          <h3>Barrierefreiheit</h3>
-          <span>
-            {currentLocation.infrastructure.wheelchair ? "ja" : "nein"}
-          </span>
-        </StyledBox>
-      </StyledBoxWrapper>
+          <h3>{currentLocation.infrastructure.wheelchair ? "ja" : "nein"}</h3>
+          <span>Barrierefreiheit</span>
+        </StyledBoxOther>
+      </StyledBoxOtherWrapper>
     </>
   );
 }
@@ -75,7 +61,7 @@ const StyledDivider = styled.aside`
 
 const StyledBoxWrapper = styled.article`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: 1fr;
   justify-items: center;
   justify-content: space-evenly;
   gap: 0.5rem;
@@ -83,6 +69,31 @@ const StyledBoxWrapper = styled.article`
 `;
 
 const StyledBox = styled.div`
+  font-size: 0.8rem;
+  height: 8rem;
+  width: 100%;
+  box-shadow: var(--box-shadow);
+  border-radius: var(--border-radius);
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 3fr 1fr;
+  grid-auto-flow: column;
+  align-items: center;
+  justify-items: center;
+  text-transform: capitalize;
+`;
+
+const StyledBoxOtherWrapper = styled.article`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  justify-items: center;
+  justify-content: space-evenly;
+  gap: 0.5rem;
+  margin-top: 1rem;
+`;
+
+const StyledBoxOther = styled.div`
   font-size: 0.8rem;
   height: 8rem;
   min-width: 8rem;
@@ -94,5 +105,3 @@ const StyledBox = styled.div`
   justify-items: center;
   text-transform: capitalize;
 `;
-
-//grid-template-columns: repeat(${columns}, 1fr);
