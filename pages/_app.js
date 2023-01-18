d923a3dc49ef72b97e00f922ed04f14d7189b902
Marvin Roellecke <marvin@roellecke.com>
2023-01-19 00:06:57 +0100

    add startup screen

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import GlobalStyles from "../components/GlobalStyles";
 import { useState, useEffect } from "react";
 import useLocalStorageState from "use-local-storage-state";
 import useCurrentPosition from "../helpers/useCurrentPosition";
+import styled, { keyframes } from "styled-components";
 
 export default function App({ Component, pageProps }) {
   const currentPosition = useCurrentPosition();
@@ -39,15 +40,50 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyles />
-      <Component
-        {...pageProps}
-        locations={locations}
-        onToggleFavorite={handleToggleFavorite}
-        favorites={favorites}
-        setLocations={setLocations}
-        startFetching={startFetching}
-        currentPosition={currentPosition}
-      />
+      {!locations || currentPosition === undefined ? (
+        <StyledLoadingScreen>
+          <StyledH1>localSports</StyledH1>
+        </StyledLoadingScreen>
+      ) : (
+        <Component
+          {...pageProps}
+          locations={locations}
+          onToggleFavorite={handleToggleFavorite}
+          favorites={favorites}
+          setLocations={setLocations}
+          startFetching={startFetching}
+          currentPosition={currentPosition}
+        />
+      )}
     </>
   );
 }
+
+const StyledLoadingScreen = styled.div`
+  width: 300px;
+  height: 100svh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+`;
+
+const typing = keyframes`
+  from { width: 0 }
+  to { width: 60% }
+`;
+
+const blinkCaret = keyframes`
+   from, to { border-color: transparent }
+  50% { border-color: black; }
+`;
+
+const StyledH1 = styled.h1`
+  overflow: hidden;
+  border-right: 0.15em solid black;
+  white-space: nowrap;
+  margin: 0 auto;
+  letter-spacing: 0.01em;
+  animation: ${typing} 1.5s steps(15, end),
+    ${blinkCaret} 0.75s step-end infinite;
+`;
